9fd003aa444880f99ed75ff48e7f259f2534b1a4
Erik Olsen <tusamni@gmail.com>
2022-08-10 17:30:21 -0500

    commit

--- src/functions/Posts.ts
+++
@@ -1,8 +0,0 @@
-export async function getPosts(limit = 4) {
-    return await import.meta.glob("../pages/blog/*.mdx");
-    
-}
-
-export async function getShoots(limit = 4) {
-    return await import.meta.glob("../pages/photography/*.mdx");
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./src/**/*.{astro,js,mdx,ts}"],
+    content: ["./src/**/*.{astro,js,mdx,ts,css}"],
     theme: {
         extend: {},
     },

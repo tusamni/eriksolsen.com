1465b1478d7f8ed6c4335cf81e4944457523339e
Erik Olsen <erik@eriksolsen.com>
2022-08-10 14:59:31 -0500

    update

---
+++ src/functions/Posts.ts
@@ -0,0 +1,7 @@
+export async function getPosts(limit = 4) {
+    return await import.meta.glob("../pages/blog/*.mdx");
+}
+
+export async function getShoots(limit = 4) {
+    return await import.meta.glob("../pages/photography/*.mdx");
+}

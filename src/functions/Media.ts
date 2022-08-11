98b9f61c9afee6bb26e2579655783104ffea6f0b
Erik Olsen <erik@eriksolsen.com>
2022-08-10 23:08:28 -0500

    prep remote images

--- src/functions/Media.ts
+++ src/functions/Media.ts
@@ -10,36 +10,26 @@ export function getPublicPath(file) {
 }
 
 // get the base directory without the filename
-export function getDirectory(file) {
-    const publicPath = getPublicPath(file);
+// export function getDirectory(file) {
+//     const publicPath = getPublicPath(file);
 
-    return path.dirname(publicPath);
-}
+//     return path.dirname(publicPath);
+// }
 
 // get the base filename without the extension
-export function getFilename(file) {
-    const publicPath = getPublicPath(file);
-    const extension = getExtension(file); // extension of the image file
+// export function getFilename(file) {
+//     const publicPath = getPublicPath(file);
+//     const extension = getExtension(file); // extension of the image file
 
-    return path.basename(publicPath, extension); // filename of the image minus the extension
-}
+//     return path.basename(publicPath, extension); // filename of the image minus the extension
+// }
 
 // get the extension of the image
-export function getExtension(file) {
-    return path.extname(file); // extension of the image file
-}
+// export function getExtension(file) {
+//     return path.extname(file); // extension of the image file
+// }
 
-// build a srcset with image sizes
-export function buildSrcSet(file, type) {
-    let dir = getDirectory(file);
-    let filename = getFilename(file);
-
-    return imageConfig.imageSizes.map(s =>
-        dir + '/' + filename + `-w${s}${type} ${s}w`
-    )
-}
-
-export async function getMetadata(file) {
+export async function getRemoteImage(file) {
     const publicPath = getPublicPath(file);
     const extension = path.extname(publicPath); // extension of the image file
     const baseFilename = path.basename(publicPath, extension); // filename of the image minus the extension
@@ -50,8 +40,7 @@ export async function getMetadata(file) {
     const response = await fetch(metaFile);
     const json = await response.json();
 
-    const imageData = {
-        "filename": path.basename(publicPath, ".jpg"),
+    return {
         "width": json.width,
         "height": json.height,
         "date": json.DateTimeOriginal,
@@ -65,11 +54,6 @@ export async function getMetadata(file) {
         "iso": json.ISO,
         "focalLength": json.FocalLength,
         "shutterSpeed": 1 / json.ExposureTime,
-    };
-
-    return imageData;
-}
-
-export async function getRemoteImage(file) {
-    const url = `${imageConfig.cloudUrl}${file}`;
+        "url": publicPath,
+    }
 }

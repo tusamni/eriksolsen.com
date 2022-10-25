249da0cfe2449661312879360dab88f4430262b4
Erik Olsen <tusamni@gmail.com>
2022-10-24 20:37:47 -0500

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: ["./src/**/*.{astro,js,mdx,ts,css}"],
     theme: {
         extend: {
             fontFamily: {
-                'sans': ["Manrope", ...defaultTheme.fontFamily.sans]
-            }
-        }
+                sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+                heading: ["RossoBold", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [
-        require('@tailwindcss/line-clamp'),
-    ],
-}
+    plugins: [require("@tailwindcss/line-clamp")],
+};

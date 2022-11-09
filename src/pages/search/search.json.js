bd64e5aaffcdd9191ed82f6111cb6a313cb9b6f0
Erik Olsen <tusamni@gmail.com>
2022-11-09 17:26:19 -0600

    commit

--- src/pages/search/search.json.js
+++ src/pages/search/search.json.js
@@ -1,19 +1,18 @@
 export async function get() {
-    let posts = await import.meta.globEager(`../**/*.md`);
+    let content = await import.meta.globEager(`../**/*.mdx`);
 
     return {
         body: JSON.stringify(
-            Object.values(posts)
-                .map((p) => {
-                    return {
-                        title: p.frontmatter.title,
-                        description: p.frontmatter.description,
-                        excerpt: p.frontmatter.excerpt,
-                        category: p.frontmatter.category,
-                        date: p.frontmatter.date,
-                        slug: p.url,
-                    };
-                }),
+            Object.values(content).map((c) => {
+                return {
+                    title: c.frontmatter.title,
+                    description: c.frontmatter.description,
+                    excerpt: c.frontmatter.excerpt,
+                    category: c.frontmatter.category,
+                    date: c.frontmatter.date,
+                    slug: c.url,
+                };
+            }),
             false,
             1
         ),

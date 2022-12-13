8c1ea8a177e396318de13106e104f3666523b53d
Erik Olsen <tusamni@gmail.com>
2022-12-12 20:23:13 -0600

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -122,7 +122,7 @@ module.exports = {
 
                     // Images
                     img: {
-                        borderRadius: theme("borderRadius.3xl"),
+                        borderRadius: theme("borderRadius.xl"),
                     },
 
                     // Inline elements

bb0d7078f6fe4c64afecf01dc439703262fc66f5
Erik Olsen <erik@eriksolsen.com>
2023-01-07 21:36:03 -0600

    add ssr and lower img requirements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,24 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
-
 import alpine from "@astrojs/alpinejs";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        shikiConfig: {
-            theme: "material-lighter",
-            wrap: true,
-        },
-    },
-    site: "https://eriksolsen.com",
-    integrations: [
-        alpine(),
-        mdx(),
-        sitemap(),
-        tailwind(),
-        image({
-            serviceEntryPoint: "@astrojs/image/sharp",
-        }),
-    ],
+  output: "server",
+  markdown: {
+    shikiConfig: {
+      theme: "material-lighter",
+      wrap: true
+    }
+  },
+  site: "https://eriksolsen.com",
+  integrations: [alpine(), mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
+  adapter: netlify()
 });

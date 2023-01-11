29aa337845f192360ae6bc3204f180017e4a428c
Erik Olsen <erik@eriksolsen.com>
2023-01-10 22:01:06 -0600

    content zod

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,12 @@ import { z, defineCollection } from 'astro:content';
 const blogs = defineCollection({
     schema: {
         title: z.string(),
+        description: z.string(),
+        excerpt: z.string(),
+        tags: z.array(z.string()).optional(),
+        category: z.string(),
+        date: z.date(),
+
   },
 });
 

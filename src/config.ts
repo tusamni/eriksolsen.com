5cc470a33deb26de369d2306c01188118522974e
Erik Olsen <tusamni@gmail.com>
2023-01-17 11:16:56 -0600

    add video thumbnail

--- src/config.ts
+++ src/config.ts
@@ -11,18 +11,3 @@ export const imageConfig = {
     imageSizes: [2000, 1440, 1080, 720, 480],
     imageFormats: ["webp", "jpg"]
 }
-
-export const searchConfig = {
-    includeScore: true,
-    includeMatches: true,
-    minMatchCharLength: 3,
-    keys: [
-        { name: "title", weight: 3 },
-        { name: "description", weight: 2 },
-        { name: "excerpt", weight: 2 }
-    ]
-}
-
-export const pageSize = 6
-export const postSize = 6 // how many posts per page
-export const photoSize = 4 // how many photo galleries per page

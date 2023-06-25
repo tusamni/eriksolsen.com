8438cf011cbe175ccc2211a25130f0782e207c42
Erik Olsen <tusamni@gmail.com>
2023-06-25 13:51:10 -0500

    commit

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,19 @@
 export const siteConfig = {
     name: "Erik Olsen",
     nameShort: "ErikOlsen",
-    description: "My name is Erik, and I'm a digital marketing expert in Austin, TX. I'm currently running the SEM and CRO operations for ICG America.",
+    description:
+        "My name is Erik, and I'm a digital marketing expert in Austin, TX. I'm currently running the SEM and CRO operations for ICG America.",
     url: "https://eriksolsen.com",
     siteRepo: "https://github.com/tusamni/astro-blog-portfolio",
     email: "erik@eriksolsen.com",
     phone: {
         base: "+18605142743",
-        formatted: "(860) 514-2743"
-    }
-}
+        formatted: "(860) 514-2743",
+    },
+};
 
 export const imageConfig = {
     cloudUrl: "https://media.eriksolsen.com/images",
     imageSizes: [2000, 1440, 1080, 720, 480],
-    imageFormats: ["webp", "jpg"]
-}
+    imageFormats: ["webp", "jpg"],
+};
---
+++ src/data/About.ts
@@ -0,0 +1,22 @@
+export const industry = [
+    "Search Engine Marketing",
+    "Search Engine Optimization",
+    "Google Shopping",
+    "Google Merchant Center",
+    "Data Analytics",
+    "Attribution Modeling",
+    "Marketing Automation",
+    "Conversion Rate Optimization",
+    "Programming",
+];
+export const tools = [
+    "Supermetrics",
+    "Google Analytics",
+    "Google Data Studio",
+    "Google Tag Manager",
+    "Visual Website Optimizer",
+    "Lucky Orange",
+    "HTML / CSS / JS",
+    "GitHub",
+    "VS Code",
+];

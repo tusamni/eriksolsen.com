676850e2b35b36bafd3bdc2e18e8b2184770c35f
Erik Olsen <tusamni@gmail.com>
2023-07-07 11:40:41 -0500

    commit

---
+++ src/functions/Url.js
@@ -0,0 +1,9 @@
+// get the public image url
+export function stringToSlug(path) {
+    return path
+        .toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, "")
+        .replace(/[\s_-]+/g, "-")
+        .replace(/^-+|-+$/g, "");
+}

3b4f982b36be1494e034b3aa416b7f22c6d5ef8d
Erik Olsen <erik@eriksolsen.com>
2023-07-10 17:54:16 -0500

    remove dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 
 module.exports = {
     content: ["./src/**/*.{astro,js,mdx,ts,css,scss}"],
-    darkMode: "class",
-    plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/container-queries")],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/container-queries"),
+    ],
 };

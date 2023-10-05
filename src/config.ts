ab66203140cc1e77eac00ba4f4f62d961baa8ba9
Erik Olsen <tusamni@gmail.com>
2023-10-05 12:21:06 -0500

    commit

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,6 @@ export const siteConfig = {
 
 export const imageConfig = {
     cloudUrl: "https://media.eriksolsen.com/images",
-    imageSizes: [480, 720],
+    imageSizes: [480, 720, 1280],
     imageFormats: ["webp", "jpg"],
 };

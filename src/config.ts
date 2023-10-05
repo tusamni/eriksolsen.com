516b8ad69f50f76cd2e96feadcb646a476367017
Erik Olsen <tusamni@gmail.com>
2023-10-05 12:04:08 -0500

    commit

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 export const siteConfig = {
     name: "Erik Olsen",
     nameShort: "ErikOlsen",
-    description:
-        "My name is Erik, and I'm a digital marketing expert in Austin, TX. I'm currently running the SEM and CRO operations for ICG America.",
+    description: "My name is Erik, and I'm a digital marketing expert in Austin, TX. I'm currently running the SEM and CRO operations for ICG America.",
     url: "https://eriksolsen.com",
     siteRepo: "https://github.com/tusamni/astro-blog-portfolio",
     email: "erik@eriksolsen.com",
@@ -14,6 +13,6 @@ export const siteConfig = {
 
 export const imageConfig = {
     cloudUrl: "https://media.eriksolsen.com/images",
-    imageSizes: [2000, 1440, 1080, 720, 480],
+    imageSizes: [480],
     imageFormats: ["webp", "jpg"],
 };

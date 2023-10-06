0a4e0ec41ebb62ab43414b88bd4e24ca2e24a838
Erik Olsen <tusamni@gmail.com>
2023-10-05 20:16:20 -0500

    commit

--- src/data/Testimonials.ts
+++ src/data/Testimonials.ts
@@ -1,12 +1,5 @@
 export const Testimonials = [
     {
-        highlight: "",
-        text: "I worked with Erik for many years, both on professional and side gigs. He's always been extremely self-motivated to continuously raise his own bar.",
-        name: "Benjamin Wright",
-        position: "Marketing Developer @ Scholly",
-        image: "/testimonials/benjamin-wright.jpg",
-    },
-    {
         highlight: "Sole Photographer",
         text: "Erik is now our sole photographer for all of our vehicles going up for auction. He was able to catch everything the E-type had to offer.",
         name: "Chris G.",

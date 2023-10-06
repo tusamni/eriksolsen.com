5cedcaaca881f1019236a31254f66dd60c022642
Erik Olsen <tusamni@gmail.com>
2023-10-06 15:40:23 -0500

    commit

--- src/data/Photography.ts
+++ src/data/Photography.ts
@@ -13,27 +13,14 @@ export const Stats = [
     },
 ];
 
-export const Gear = [
-    "Sony A7iii",
-    "Tamron 20-40mm f/2.8 Di III VXD",
-    "Tamron 35-150mm f/2-2.8 Di III VXD",
-    "Tiffen Polarizing Filters",
-    "DJI Osmo Action 3",
-    "DJI Wireless Mic",
-    "iFootage Mounting Gear",
-    "Nanuk 935 Case",
-];
-
 export const Faq = [
     {
         heading: "Will you come to my location?",
-        description:
-            "Absolutely. I'll traveled all over central Texas for shoots. I'm currently located in Dripping Springs, but have no problem traveling. Within a reasonable distance, I'll do it at no charge. If it's outside the Austin area, I'll ask for travel expenses.",
+        description: "Absolutely. I'll traveled all over central Texas for shoots. I'm currently located in Dripping Springs, but have no problem traveling. Within a reasonable distance, I'll do it at no charge. If it's outside the Austin area, I'll ask for travel expenses.",
     },
     {
         heading: "How much does a photo shoot cost?",
-        description:
-            "Well, it depends. I can build a photoshoot package to meet any needs. Generally speaking, a full photoshoot (200 photos) with video (cold-start, walk-around and driving) for an online auction costs $1,000.",
+        description: "Well, it depends. I can build a photoshoot package to meet any needs. Generally speaking, a full photoshoot (200 photos) with video (cold-start, walk-around and driving) for an online auction costs $1,000.",
     },
     {
         heading: "What should I expect on the shoot?",

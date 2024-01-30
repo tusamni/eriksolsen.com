d5ee50c8671f0e777475daf17102c1d253c01ebf
Erik Olsen <erik@eriksolsen.com>
2024-01-30 17:31:40 -0600

    update blog and typography settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,5 +2,18 @@
 
 module.exports = {
     content: ["./src/**/*.{astro,js,mdx,ts,css,scss}"],
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        p: {
+                            letterSpacing: "-0.025em",
+                        },
+                    },
+                },
+            },
+        },
+    },
     plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/container-queries")],
 };

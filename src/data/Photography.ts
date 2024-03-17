07a6c13116dfe21afde6bcf3d7bcaf8b75961083
Erik Olsen <erik@eriksolsen.com>
2024-03-16 21:20:36 -0500

    commit

--- src/data/Photography.ts
+++ src/data/Photography.ts
@@ -23,9 +23,20 @@ export const Testimonials = [
     {
         text: "Erik is now our sole photographer for all vehicles going up for auction. He was able to capture everything the Jaguar E-type had to offer.",
         name: "Chris G",
-        position: "MFE Enterprises",
         vehicle: "1969 Jaguar XKE Series II",
         image: "/testimonials/chris-glosson.jpg",
+    },
+    {
+        text: "Erik's professionalism and expertise were on display from our first interaction onward. He was responsive and flexible while we planned the photo shoot. When it came time to shoot, Erik followed the shot list closely and went above and beyond with additional photos of my vehicle. After shooting, Erik promptly made the necessary edits and shared the photos. Hiring Erik positioned me to sell my vehicle at top dollar compared to the market average; I highly recommend him!",
+        name: "Connor C.",
+        vehicle: "2000 Toyota 4Runner",
+        image: "/testimonials/connor-chittock.jpg",
+    },
+    {
+        text: "Recently I listed my 1970 Porsche 911T on Hagerty. Upon meeting Erik I was pleased to find an easy to deal with gentleman with an open attitude towards what I was wanting to achieve from the photography of my 911T.  Erik is very friendly, punctual, and made the outdoors photo session very enjoyable. The photography results were all I could have wanted. Erik chose the proper time of day to shoot, positioned my 911T to get the correct depth of field, and I credit Erik with helping me sell my car at full reserve.",
+        name: "Frank B.",
+        vehicle: "1970 Porsche 911T",
+        image: "/testimonials/frank-b.jpg",
     },
 ];
 

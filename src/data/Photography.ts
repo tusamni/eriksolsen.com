55a22068a15f2e51b9ac0ef0f08e6ad5bdbcb72d
Erik Olsen <erik@eriksolsen.com>
2024-03-20 18:08:08 -0500

    commit

--- src/data/Photography.ts
+++ src/data/Photography.ts
@@ -21,19 +21,28 @@ export const Faq = [
 
 export const Testimonials = [
     {
-        text: "Erik is now our sole photographer for all vehicles going up for auction. He was able to capture everything the Jaguar E-type had to offer.",
+        text: {
+            short: "Erik is now our sole photographer for all vehicles going up for auction. He was able to capture everything the Jaguar E-type had to offer.",
+            long: "Erik is now our sole photographer for all vehicles going up for auction. He was able to capture everything the Jaguar E-type had to offer.",
+        },
         name: "Chris G",
         vehicle: "1969 Jaguar XKE Series II",
         image: "/testimonials/chris-glosson.jpg",
     },
     {
-        text: "Erik's professionalism and expertise were on display from our first interaction onward. He was responsive and flexible while we planned the photo shoot. When it came time to shoot, Erik followed the shot list closely and went above and beyond with additional photos of my vehicle. After shooting, Erik promptly made the necessary edits and shared the photos. Hiring Erik positioned me to sell my vehicle at top dollar compared to the market average; I highly recommend him!",
+        text: {
+            short: "Hiring Erik positioned me to sell my vehicle at top dollar compared to the market average; I highly recommend him!",
+            long: "Erik's professionalism and expertise were on display from our first interaction onward. He was responsive and flexible while we planned the photo shoot. When it came time to shoot, Erik followed the shot list closely and went above and beyond with additional photos of my vehicle. After shooting, Erik promptly made the necessary edits and shared the photos. Hiring Erik positioned me to sell my vehicle at top dollar compared to the market average; I highly recommend him!",
+        },
         name: "Connor C.",
         vehicle: "2000 Toyota 4Runner",
         image: "/testimonials/connor-chittock.jpg",
     },
     {
-        text: "Recently I listed my 1970 Porsche 911T on Hagerty. Upon meeting Erik I was pleased to find an easy to deal with gentleman with an open attitude towards what I was wanting to achieve from the photography of my 911T.  Erik is very friendly, punctual, and made the outdoors photo session very enjoyable. The photography results were all I could have wanted. Erik chose the proper time of day to shoot, positioned my 911T to get the correct depth of field, and I credit Erik with helping me sell my car at full reserve.",
+        text: {
+            short: "Erik is very friendly, punctual, and made the outdoors photo session very enjoyable. The photography results were all I could have wanted.",
+            long: "Recently I listed my 1970 Porsche 911T on Hagerty. Upon meeting Erik I was pleased to find an easy to deal with gentleman with an open attitude towards what I was wanting to achieve from the photography of my 911T.  Erik is very friendly, punctual, and made the outdoors photo session very enjoyable. The photography results were all I could have wanted. Erik chose the proper time of day to shoot, positioned my 911T to get the correct depth of field, and I credit Erik with helping me sell my car at full reserve.",
+        },
         name: "Frank B.",
         vehicle: "1970 Porsche 911T",
         image: "/testimonials/frank-b.jpg",
@@ -61,4 +70,4 @@ export const Pricing = [
     },
 ];
 
-export const auctionSites = ["bring-a-trailer", "cars-and-bids", "ebay", "hagerty", "hemmings", "zero-260"];
+export const auctionSites = ["bring-a-trailer", "cars-and-bids", "ebay", "hagerty", "hemmings"];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,139 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
     content: ["./src/**/*.{astro,js,ts,mdx,ts,css,scss}"],
     theme: {
-        extend: {
-            typography: {
-                DEFAULT: {
-                    css: {
-                        p: {
-                            letterSpacing: "-0.025em",
-                        },
-                    },
+        fontSize: {
+            xs: [
+                "0.75rem",
+                {
+                    lineHeight: "1rem",
                 },
+            ],
+            sm: [
+                "0.875rem",
+                {
+                    lineHeight: "1.5rem",
+                },
+            ],
+            base: [
+                "1rem",
+                {
+                    lineHeight: "1.75rem",
+                },
+            ],
+            lg: [
+                "1.125rem",
+                {
+                    lineHeight: "2rem",
+                },
+            ],
+            xl: [
+                "1.25rem",
+                {
+                    lineHeight: "2rem",
+                },
+            ],
+            "2xl": [
+                "1.5rem",
+                {
+                    lineHeight: "2rem",
+                },
+            ],
+            "3xl": [
+                "2rem",
+                {
+                    lineHeight: "2.5rem",
+                },
+            ],
+            "4xl": [
+                "2.5rem",
+                {
+                    lineHeight: "3.5rem",
+                },
+            ],
+            "5xl": [
+                "3rem",
+                {
+                    lineHeight: "3.5rem",
+                },
+            ],
+            "6xl": [
+                "3.75rem",
+                {
+                    lineHeight: "1",
+                },
+            ],
+            "7xl": [
+                "4.5rem",
+                {
+                    lineHeight: "1.1",
+                },
+            ],
+            "8xl": [
+                "6rem",
+                {
+                    lineHeight: "1",
+                },
+            ],
+            "9xl": [
+                "8rem",
+                {
+                    lineHeight: "1",
+                },
+            ],
+        },
+        extend: {
+            colors: {
+                black: "#0C1824",
+                regent: {
+                    50: "#F4F9FB",
+                    100: "#E9F3F6",
+                    200: "#D7E9EF",
+                    300: "#C1DCE6",
+                    400: "#AFD2DF",
+                    500: "#99C6D6",
+                    600: "#66A9C2",
+                    700: "#3F839C",
+                    800: "#2A596A",
+                    900: "#142B33",
+                },
+                lila: {
+                    50: "#F8F8FC",
+                    100: "#F1F2F9",
+                    200: "#DFE2F2",
+                    300: "#CDD1EA",
+                    400: "#B3BBE0",
+                    500: "#9CA6D6",
+                    600: "#8894CE",
+                    700: "#7683C6",
+                    800: "#5969BA",
+                    900: "#3C4A90",
+                },
+                yellow: {
+                    50: "#FFFCFA",
+                    100: "#FFFAF5",
+                    200: "#FFF5EB",
+                    300: "#FFF0E0",
+                    400: "#FFE8D1",
+                    500: "#FFE3C7",
+                    600: "#FFC78F",
+                },
+                pink: {
+                    50: "#FEFBFB",
+                    100: "#FDF6F8",
+                    200: "#FBEAED",
+                    300: "#F9DDE2",
+                    400: "#F7D4DA",
+                    500: "#F4C4CD",
+                    600: "#EFA9B6",
+                },
+            },
+            fontFamily: {
+                sans: ["General Sans", ...defaultTheme.fontFamily.sans],
             },
         },
     },

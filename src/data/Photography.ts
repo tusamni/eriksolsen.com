820ec03849a7db9783cd233330b93d1a18e37555
Erik Olsen <erik@eriksolsen.com>
2024-03-25 22:33:37 -0500

    commit

--- src/data/Photography.ts
+++ src/data/Photography.ts
@@ -1,21 +1,31 @@
 export const Faq = [
     {
-        title: "What should I expect on the shoot?",
-        description:
-            "<p>We'll generally try to meet up around sunrise or sunset for the best lighting. A basic photos-only shoot takes around 2 hours, if we're doing video add another hour at a minimum. You're more than welcome to stay with me during the shoot, but it's not required.</p><p>You should ensure the car is very well detailed as any dirt, smudges are likely to show up in the photos. I'll ask you to walk me through any specific details on the car, things you'd like to highlight, etc.</p>",
+        category: "general",
+        questions: [
+            {
+                title: "What should I expect on the shoot?",
+                description:
+                    "<p>We'll generally try to meet up around sunrise or sunset for the best lighting. A basic photos-only shoot takes around 2 hours, if we're doing video add another hour at a minimum. You're more than welcome to stay with me during the shoot, but it's not required.</p><p>You should ensure the car is very well detailed as any dirt, smudges are likely to show up in the photos. I'll ask you to walk me through any specific details on the car, things you'd like to highlight, etc.</p>",
+            },
+            {
+                title: "Will you come to my location?",
+                description:
+                    "<p>Absolutely. I'll traveled all over Texas for shoots. I'm currently located in Austin, but have no problem traveling. Within a reasonable distance, I'll do it at no charge. If it's outside the Austin area, I'll ask for travel expenses.</p>",
+            },
+            {
+                title: "How much does a shoot cost?",
+                description: `<p>The short answer: it depends. That said, I'll do my best to work within your set budget. I can build a photoshoot package to meet any needs.</p><p>If you'd like more specific details, check out my <a href="/photography/pricing" title="Car photography pricing" class="underline">car photography pricing.</a></p>`,
+            },
+        ],
     },
     {
-        title: "Will you come to my location?",
-        description:
-            "<p>Absolutely. I'll traveled all over Texas for shoots. I'm currently located in Austin, but have no problem traveling. Within a reasonable distance, I'll do it at no charge. If it's outside the Austin area, I'll ask for travel expenses.</p>",
-    },
-    {
-        title: "How much does a photo shoot cost?",
-        description: `<p>The short answer: it depends. That said, I'll do my best to work within your set budget. I can build a photoshoot package to meet any needs.</p><p>If you'd like more specific details, check out my <a href="/photography/pricing" title="Car photography pricing" class="underline">car photography pricing.</a></p>`,
-    },
-    {
-        title: "What gear are you using?",
-        description: `<p>I've changed gear many times. That said, I'm really pleased with my current setup.</p><ul class="text-lg ml-6"><li>Sony Alpha A7iii</li><li>20-40mm f/2.8 Di III VXD</li><li>35-150mm f/2-2.8 Di III VXD</li></ul><p>If you'd like a more detailed explanation of what equipment I'm using, check out my <a href="/photography/gear" title="Car photography gear" class="underline">car photography gear</a> write-up.</p>`,
+        category: "photography",
+        questions: [
+            {
+                title: "What gear are you using?",
+                description: `<p>I've changed gear many times. That said, I'm really pleased with my current setup.</p><ul class="text-lg ml-6"><li>Sony Alpha A7iii</li><li>20-40mm f/2.8 Di III VXD</li><li>35-150mm f/2-2.8 Di III VXD</li></ul><p>If you'd like a more detailed explanation of what equipment I'm using, check out my <a href="/photography/gear" title="Car photography gear" class="underline">car photography gear</a> write-up.</p>`,
+            },
+        ],
     },
 ];
 

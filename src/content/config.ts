6f12f598a816fbbb62da69aa950a6998e8370e6e
Erik Olsen <erik@eriksolsen.com>
2024-04-01 15:42:00 -0500

    commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -59,6 +59,7 @@ const shoots = defineCollection({
             })
             .optional(),
         images: z.object({
+            columns: z.string().optional().default("h-[25em]"),
             hero: z.string(),
             gallery: z.array(z.string()),
         }),

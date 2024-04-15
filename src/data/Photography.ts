d99756315f00607970d781bcee2d717fa87c15ef
Erik Olsen <erik@eriksolsen.com>
2024-04-15 18:01:37 -0500

    commit

--- src/data/Photography.ts
+++ src/data/Photography.ts
@@ -45,6 +45,7 @@ export const Faq = [
 export const Testimonials = [
     {
         text: {
+            headline: "Our Sole Photographer",
             short: "Erik is now our sole photographer for all vehicles going up for auction. He was able to capture everything the Jaguar E-type had to offer.",
             long: "Erik is now our sole photographer for all vehicles going up for auction. He was able to capture everything the Jaguar E-type had to offer.",
         },
@@ -54,6 +55,7 @@ export const Testimonials = [
     },
     {
         text: {
+            headline: "Went Above and Beyond",
             short: "Hiring Erik positioned me to sell my vehicle at top dollar compared to the market average; I highly recommend him!",
             long: "Erik's professionalism and expertise were on display from our first interaction onward. He was responsive and flexible while we planned the photo shoot. When it came time to shoot, Erik followed the shot list closely and went above and beyond with additional photos of my vehicle. After shooting, Erik promptly made the necessary edits and shared the photos. Hiring Erik positioned me to sell my vehicle at top dollar compared to the market average; I highly recommend him!",
         },
@@ -63,6 +65,7 @@ export const Testimonials = [
     },
     {
         text: {
+            headline: "All I Could Have Wanted",
             short: "Erik is very friendly, punctual, and made the outdoors photo session very enjoyable. The photography results were all I could have wanted.",
             long: "Recently I listed my 1970 Porsche 911T on Hagerty. Upon meeting Erik I was pleased to find an easy to deal with gentleman with an open attitude towards what I was wanting to achieve from the photography of my 911T.  Erik is very friendly, punctual, and made the outdoors photo session very enjoyable. The photography results were all I could have wanted. Erik chose the proper time of day to shoot, positioned my 911T to get the correct depth of field, and I credit Erik with helping me sell my car at full reserve.",
         },

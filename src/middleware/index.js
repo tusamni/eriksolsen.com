a91fb188e21ae1f766801bd58ce772cd65ac523c
Erik Olsen <erik@eriksolsen.com>
2024-04-22 22:22:53 -0500

    add cookies

---
+++ src/middleware/index.js
@@ -0,0 +1,25 @@
+export function onRequest({ url, cookies }, next) {
+    if (url.searchParams.get("paid") && url.searchParams.get("source")) {
+        cookies.set("paid_source", url.searchParams.get("source")); // set the source of the click
+
+        // grab the click identifier from the params so that we can upload offline conversions later
+        switch (url.searchParams.get("source")) {
+            case "google":
+                if (url.searchParams.get("gclid")) {
+                    cookies.set("paid_clickid", url.searchParams.get("gclid"));
+                }
+
+                break;
+
+            case "microsoft":
+                if (url.searchParams.get("gclid")) {
+                    cookies.set("paid_clickid", url.searchParams.get("msclkid"));
+                }
+
+                break;
+        }
+    }
+
+    // return a Response or the result of calling `next()`
+    return next();
+}

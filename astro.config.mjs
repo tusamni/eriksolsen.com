9c9dd8787ac031ffc12d3d596f571699ba311810
erik <erik@eriksolsen.com>
2024-06-14 16:53:41 +0000

    add image pages back into site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
 import alpine from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
@@ -42,7 +41,6 @@ export default defineConfig({
         "/photography/2021-pca-event/": "/photography/shoots/2021/06/hill-country-pca-event/",
         "/photography/2021-porsche-930-turbo-flachbau/": "/photography/shoots/2021/06/1989-porsche-930-turbo-flachbau/",
         "/photography/2022-lit-show-day-one/": "/photography/shoots/2022/02/lit-show-day-one/",
-        "/photography/2022-backdraft-cobra/": "/photography/shoots/2022/11/1965-backdraft-cobra/",
-        "/photography/photo/[...slug]": "/"
+        "/photography/2022-backdraft-cobra/": "/photography/shoots/2022/11/1965-backdraft-cobra/"
     },
 });

e1b7752d373a9b0777e71b8dd651abd453943d8b
erik <erik@eriksolsen.com>
2024-07-16 22:53:43 +0000

    commit

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,36 @@
-export function onRequest({ url, cookies }, next) {
+import { supabase } from "@/library/supabase";
+
+export async function onRequest({ url, cookies, redirect }, next) {
+    // check for a protected path requiring login    
+    const protectedPaths = [
+        "dashboard"
+    ]
+
+    if (protectedPaths.some((string) => url.pathname.includes(string))) {
+        const accessToken = cookies.get("sb-access-token");
+        const refreshToken = cookies.get("sb-refresh-token");
+
+        if (!accessToken || !refreshToken) {
+            return redirect("/login");
+        }
+
+        const { data, error } = await supabase.auth.setSession({
+            refresh_token: refreshToken.value,
+            access_token: accessToken.value,
+        });
+
+        if (error) {
+            cookies.delete("sb-access-token", {
+                path: "/",
+            });
+            cookies.delete("sb-refresh-token", {
+                path: "/",
+            });
+
+            return redirect("/login");
+        }
+    }
+
     const validParams = [
         "utm_source",
         "utm_medium",

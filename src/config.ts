1be0c95e34d000df7df18e0dfe5b493cb76be244
erik <erik@eriksolsen.com>
2024-09-25 21:10:30 +0000

    commit

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,18 @@
 export const imageConfig = {
-    cloudUrl: "https://media.eriksolsen.com",
-    imageSizes: [4000, 3000, 2000, 1800, 1400, 1200, 1000, 900, 800, 700, 600, 500],
+	cloudUrl: "https://media.eriksolsen.com",
+	imageSizes: [4000, 3000, 2000, 1800, 1400, 1200, 1000, 900, 800, 700, 600, 500, 250],
 };
 
 export const siteConfig = {
-    name: "Erik Olsen",
-    nameShort: "ErikOlsen",
-    phone: {
-        base: "+15126090471",
-        formatted: "(512) 609-0471",
-    },
-    email: {
-        base: "erik@eriksolsen.com",
-    },
-    url: "https://eriksolsen.com",
-    description: "My name is Erik, and I'm a digital marketing expert in Austin, TX. I'm currently running the SEM and CRO operations for ICG America.",
+	name: "Erik Olsen",
+	nameShort: "ErikOlsen",
+	phone: {
+		base: "+15126090471",
+		formatted: "(512) 609-0471",
+	},
+	email: {
+		base: "erik@eriksolsen.com",
+	},
+	url: "https://eriksolsen.com",
+	description: "My name is Erik, and I'm a digital marketing expert in Austin, TX. I'm currently running the SEM and CRO operations for ICG America.",
 };

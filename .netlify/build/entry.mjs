5267227e1f7bba0a92b4ed48219f63b6919e2c45
erik <erik@eriksolsen.com>
2024-09-26 22:42:06 +0000

    commit

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,46 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_B57nqq2C.mjs';
+import { onRequest } from './_noop-middleware.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/404.astro.mjs');
+const _page2 = () => import('./pages/blog/_slug_.astro.mjs');
+const _page3 = () => import('./pages/blog.astro.mjs');
+const _page4 = () => import('./pages/photography/faq.astro.mjs');
+const _page5 = () => import('./pages/photography/pricing.astro.mjs');
+const _page6 = () => import('./pages/photography.astro.mjs');
+const _page7 = () => import('./pages/privacy.astro.mjs');
+const _page8 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/404.astro", _page1],
+    ["src/pages/blog/[slug].astro", _page2],
+    ["src/pages/blog/index.astro", _page3],
+    ["src/pages/photography/faq.astro", _page4],
+    ["src/pages/photography/pricing.astro", _page5],
+    ["src/pages/photography/index.astro", _page6],
+    ["src/pages/privacy.astro", _page7],
+    ["src/pages/index.astro", _page8]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "4747e7da-9acd-4a74-b513-89e73ef557cb"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };

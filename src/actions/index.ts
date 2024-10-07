4762b517527db4ee00190724bfa9ee42b0578edb
erik <erik@eriksolsen.com>
2024-10-07 22:05:44 +0000

    commit

---
+++ src/actions/index.ts
@@ -0,0 +1,77 @@
+import { defineAction, ActionError } from 'astro:actions';
+import { z } from 'astro:schema';
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+import { siteConfig } from "@/config";
+
+export const server = {
+	contact: defineAction({
+		accept: "form",
+		input: z.object({
+			name: z.string(),
+			email: z.string().email(),
+			phone: z.string().optional(),
+			zip: z.string().optional(),
+			message: z.string().optional(),
+			path: z.string()
+		}),
+		handler: async (input) => {
+			if (input.zip) {
+				throw new ActionError({
+					code: "SPAM_ERROR",
+					message: "There's been an error. Please try again later.",
+				});
+			}
+
+			// new instance of mailersend
+			const mailerSend = new MailerSend({
+				apiKey: import.meta.env.MAILERSEND_TOKEN,
+			});
+			const sentFrom = new Sender(siteConfig.email.base, siteConfig.name);
+
+			// set personalization variables
+			const leadSubject = `New Lead from eriksolsen.com - ${input.name}`;
+			const leadRecipients = [new Recipient(siteConfig.email.base, siteConfig.name)];
+			const leadReplyTo = new Sender(input.email, input.name);
+			const leadPersonalization = [
+				{
+					email: siteConfig.email.base,
+					data: {
+						name: input.ame,
+						email: input.email,
+						phone: input.phone,
+						message: input.message,
+						path: input.path
+					},
+				},
+			];
+			const leadParams = new EmailParams().setFrom(sentFrom).setTo(leadRecipients).setReplyTo(leadReplyTo).setSubject(leadSubject).setPersonalization(leadPersonalization).setTemplateId("3z0vklo0r3el7qrx");
+
+			// send the contact email
+			try {
+				await mailerSend.email.send(leadParams);
+			} catch (error) {
+				console.error(error);
+			}
+
+			const thanksSubject = `Thanks ${input.name}, I've Received Your Message!`;
+			const thanksRecipients = [new Recipient(input.email, input.name)];
+			const thanksReplyTo = new Sender(siteConfig.email.base, siteConfig.name);
+			const thanksPersonalization = [
+				{
+					email: input.email,
+					data: {
+						name: input.name
+					}
+				}
+			]
+			const thanksParams = new EmailParams().setFrom(sentFrom).setTo(thanksRecipients).setReplyTo(thanksReplyTo).setSubject(thanksSubject).setPersonalization(thanksPersonalization).setTemplateId("v69oxl51n6x4785k");
+
+			// send the thanks email
+			try {
+				await mailerSend.email.send(thanksParams);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	})
+}

be53ac20a6574852efaafeadc0f03918b4c6604f
erik <erik@eriksolsen.com>
2024-12-17 21:52:11 +0000

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import alpine from "@astrojs/alpinejs";
-import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -10,12 +9,6 @@ export default defineConfig({
 	adapter: netlify(),
 	integrations: [
 		alpine(),
-		mdx({
-			syntaxHighlight: "shiki",
-			shikiConfig: {
-				theme: "dracula-soft"
-			}
-		}),
 		sitemap(),
 		tailwind({ applyBaseStyles: true })
 	],

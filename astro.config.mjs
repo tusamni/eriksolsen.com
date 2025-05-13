e0f3d4a9c5752457eb801c2a4a6e70ee64053246
erik <erik@eriksolsen.com>
2025-05-13 19:29:50 +0000

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import alpine from "@astrojs/alpinejs";
 import clerk from '@clerk/astro'
 import netlify from "@astrojs/netlify";
@@ -8,6 +8,45 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	adapter: netlify(),
+	experimental: {
+		fonts: [{
+			provider: "local",
+			name: "GeneralSans",
+			cssVariable: "--font-generalsans",
+			variants: [
+				{
+					weight: 200,
+					style: "normal",
+					src: ["./src/assets/fonts/GeneralSans-Extralight.woff2"]
+				},
+				{
+					weight: 300,
+					style: "normal",
+					src: ["./src/assets/fonts/GeneralSans-Light.woff2"]
+				},
+				{
+					weight: 400,
+					style: "normal",
+					src: ["./src/assets/fonts/GeneralSans-Regular.woff2"]
+				},
+				{
+					weight: 500,
+					style: "normal",
+					src: ["./src/assets/fonts/GeneralSans-Medium.woff2"]
+				},
+				{
+					weight: 600,
+					style: "normal",
+					src: ["./src/assets/fonts/GeneralSans-Semibold.woff2"]
+				},
+				{
+					weight: 700,
+					style: "normal",
+					src: ["./src/assets/fonts/GeneralSans-Bold.woff2"]
+				}
+			]
+		}]
+	},
 	integrations: [
 		alpine(),
 		clerk(),
